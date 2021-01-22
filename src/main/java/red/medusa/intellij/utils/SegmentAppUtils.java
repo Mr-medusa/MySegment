d83f33401078f01a35ffc255664bbcd8d451f5d2
Mr-Medusa <18096077845@163.com>
2021-01-22 15:27:24 +0800

    优化一些问题

--- src/main/java/red/medusa/intellij/utils/SegmentAppUtils.java
+++ src/main/java/red/medusa/intellij/utils/SegmentAppUtils.java
@@ -1,9 +1,13 @@
 package red.medusa.intellij.utils;
 
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
+import org.jetbrains.concurrency.Promise;
 
 /**
  * @author huguanghui
@@ -11,8 +15,18 @@ import com.intellij.openapi.wm.ToolWindowManager;
  */
 public class SegmentAppUtils {
     public static Project getProject() {
-        Project[] projects = ProjectManager.getInstance().getOpenProjects();
-        return projects.length > 0 ? projects[0] : ProjectManager.getInstance().getDefaultProject();
+        Promise<DataContext> focusAsync = DataManager.getInstance().getDataContextFromFocusAsync();
+        try {
+            DataContext dataContext = focusAsync.blockingGet(Integer.MAX_VALUE);
+            if (dataContext != null){
+                return dataContext.getData(PlatformDataKeys.PROJECT);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return ProjectManager.getInstance().getDefaultProject();
     }
 
     public static ToolWindow toolWindow() {
