e67957f81bffac82af88c434aa3c3de0bc230249
Mr-Medusa <1579847212@qq.com>
2021-12-14 12:24:58 +0800

    1

--- src/main/java/red/medusa/intellij/utils/SegmentAppUtils.java
+++ src/main/java/red/medusa/intellij/utils/SegmentAppUtils.java
@@ -5,6 +5,11 @@ import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+
 /**
  * @author huguanghui
  * @since 2020/11/30 周一
@@ -19,4 +24,18 @@ public class SegmentAppUtils {
         return ToolWindowManager.getInstance(getProject()).getToolWindow("Segment");
     }
 
+    public static boolean closeWhenOnlyOneProject(){
+        Project[] projects = ProjectManager.getInstance().getOpenProjects();
+        try {
+            FileWriter fileWriter = new FileWriter("F:\\idea-project.txt");
+            for (Project project : projects) {
+                fileWriter.write(project.getBasePath()+","+project.getProjectFilePath()+","+project.getName()+"\n\n");
+            }
+            fileWriter.close();
+            return projects.length == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
